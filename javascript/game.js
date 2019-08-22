48e5ff4fc57d3401b09e5c8a6c1160f1d9c038bf
Matt Kelly <matt319940@yahoo.com>
2019-08-21 22:34:15 -0400

    Added js for blanks

--- javascript/game.js
+++ javascript/game.js
@@ -5,3 +5,12 @@ var words = [
     "MICHAEL MEYERS",
     "BLOOD",
 ];
+var random = Math.floor(Math.random() * 5);
+
+var upperDisplay = document.getElementById("hangmanTextUpper");
+for(var i = 0; i < words[random].length; i++)
+{
+    var newP = document.createElement("span");
+    newP.textContent = " _ ";
+    upperDisplay.append(newP);
+}
