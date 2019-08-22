0b49a1c4ea6f282e3974272c189c8f914a978d0a
Matt Kelly <matt319940@yahoo.com>
2019-08-22 07:29:15 -0400

    Added random word generator

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Word-Guess-Game
+
+1. Create an array of "scary" words
+2. Create a variable that is randomly assigned one of the words from the words array
+3. Create a function that displays the blanks of the word
+    3a. must be able to run every time letter is guessed so it refreshes and displays the letter that is guessed
+4. 
--- javascript/game.js
+++ javascript/game.js
@@ -1,16 +1,37 @@
+// Array of random "scary" words
 var words = [
     "MURDER",
-    "FREDDY KRUEGER",
+    "FREDDY",
     "JASON",
-    "MICHAEL MEYERS",
+    "MICHAEL",
     "BLOOD",
+    "SILENCE",
+    "DECAPITATION",
+    "STRANGULATION",
+    "ASPHYXIATION",
+    "NIGHTMARE",
+    "SHADOWS",
+    "FRIGHTENED",
+    "SCREAMS",
+    "CURSED",
+    "INTRUDER",
+    "ASSASSIN",
+    "TAXES",
+    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH",
+    "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEK",
 ];
-var random = Math.floor(Math.random() * 5);
 
-var upperDisplay = document.getElementById("hangmanTextUpper");
-for(var i = 0; i < words[random].length; i++)
-{
-    var newP = document.createElement("span");
-    newP.textContent = " _ ";
-    upperDisplay.append(newP);
-}
+// Creates a random number from 1 to the number of items in the words array
+var randomNumber = Math.floor(Math.random() * words.length);
+
+// Assigns that random number as an index of the words array and stores that word
+var randomWord = words[randomNumber];
+console.log(randomWord);
+
+// var upperDisplay = document.getElementById("hangmanTextUpper");
+// for(var i = 0; i < words[randomNumber].length; i++)
+// {
+//     var newP = document.createElement("span");
+//     newP.textContent = " _ ";
+//     upperDisplay.append(newP);
+// }
