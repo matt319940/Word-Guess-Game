69158be544c207adaada09a6fad547431b0b6eed
Matt Kelly <matt319940@yahoo.com>
2019-08-21 20:13:54 -0400

    Added home page text, and js

--- javascript/game.js
+++ javascript/game.js
@@ -1,1 +1,8 @@
 //Javascript
+document.onclick = function(even){
+    document.getElementById("text").textContent = "Game On";
+    document.getElementById("btn").innerHTML = 
+    '<a href="hangman.html" role="button" type="button" class="spooky btn btn-primary btn-lg" style="margin: auto; display: block; background: rgba(0, 0, 0, 0.5); border: none;">Click me if you dare</button>'
+   
+}
+
