2ae1fda7d87d09317d25ba11cb3f573cfc8ea425
matt319940 <52135725+matt319940@users.noreply.github.com>
2019-08-26 17:03:36 -0400

    Add files via upload

--- javascript/game.js
+++ javascript/game.js
@@ -8,88 +8,103 @@ var attempts = 6;
 var wins = 0;
 var wrong = 0;
 
+
 // Creates a random number from 1 to the number of items in the words array
 var randomNumber = Math.floor(Math.random() * words.length);
 
 // Assigns that random number as an index of the words array and stores that word
 var randomWord = words[randomNumber];
 console.log("The random word is: " + randomWord);
-// console.log("randomWord.length = " + randomWord.length);
 
 // Array for hidden letters
 var hiddenLetters = [];
 
 // Array and variable for letters guessed
 var guessedLetters = [];
-var letter;
 var counter = 0;
 
-// Function for initializing the game
+// Function for initializing upper display area
 function initialize(){
 	
+	// displays " _ " for every letter in hiddenLetters
 	for(var i = 0; i < randomWord.length; i++){
 		hiddenLetters.push(" _ ");
-		var newSpan = document.createElement("span");
-		newSpan.setAttribute("class", i);
-		newSpan.textContent = hiddenLetters[i];
-		document.getElementById("hangmanTextUpper").append(newSpan);
+		document.getElementById("hangmanTextUpper").textContent += hiddenLetters[i];
 	}
 }
 
 // Function to diplay correctly guessed letters
-function hiddenRefresh(){
+function refresh(){
+	
+	// clears upper text are
+	document.getElementById("hangmanTextUpper").textContent = "";
+	
+	// displays the current state of hiddenLetters
 	for(var i = 0; i < randomWord.length; i++){
-		if(hiddenLetters[i] !== " _ "){
-			document.querySelector("span[class=" + CSS.escape(i) + "]").textContent = hiddenLetters[i];
-		}
+
+		document.getElementById("hangmanTextUpper").textContent += hiddenLetters[i];
+
 	}
 }	
+
 // Function that checks if you won
 function ifWon(){
-	var x = 1;
-	for(var i = 0; i < randomWord.length; i++){
-		if(hiddenLetters[i] == " _ "){
-			x = 0;
-		}
-	}
+	
 	var playAgain = false;
-	if(x == 1){
+	if(randomWord == hiddenLetters.join("")){
 		setTimeout(function(){ playAgain = confirm("You Won! Play again?"); }, 50);
 		setTimeout(function(){ if(playAgain) location.reload();}, 51);
-		// wins++;
-		// console.log("wins = " + wins);
 	}
+
 }
 
 // Function that checks if you lost
-function ifLost(){
-	if(wrong == attempts)
-	{
-		alert("You lost!");
+function ifLost(correct){
+	
+	var dup = isDuplicate();		
+	if(correct == 0 && dup !== true)
+		wrong++;
+	
+	document.getElementById("wrong").textContent = "Remaining Attempts: " + (attempts - wrong);
+		
+	if((attempts - wrong) == 0 ){
+		alert("You lose");
 		location.reload();
+		}
+	
+}
+
+// Checks for duplicates
+function isDuplicate(){
+	for(var i = 0; i < guessedLetters.length - 1; i++){
+		if(guessedLetters[counter] == guessedLetters[i])
+			return true;
 	}
 }
 
-// Function to get keypress
+// Function to run all other functions upon keyPress
 function keyPress(){
 
 	guessedLetters[counter] = event.key.toUpperCase();
-	document.getElementById("hangmanTextLower").textContent = guessedLetters;
-	counter++;
+	document.getElementById("hangmanTextLower").textContent = "Guessed Letters: " + guessedLetters;
+	
+	var correct = 0;
 
 	
-	for(var i = 0; i < guessedLetters.length; i++){
-		for(var j = 0; j < randomWord.length; j++){
-			if(randomWord.charAt(j) == guessedLetters[i]){
-				hiddenLetters[j] = guessedLetters[i];
-				}
+	for(var i = 0; i < randomWord.length; i++){
+		if(guessedLetters[counter] == randomWord.charAt(i)){
+			hiddenLetters[i] = guessedLetters[counter];	
+			correct++;
 			}
 	}
-	console.log("Counter = " + counter);
-	// console.log("hiddenLetters array = " + hiddenLetters);
-	hiddenRefresh();
+	
+	
+
+	refresh();
 	ifWon();
-	ifLost();
+	ifLost(correct);
+	
+	counter++;
 }
 
 //Function Calls
